c3958717eb057b4bbaa59fffe29727f7f42743b0
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-21 01:54:41 +0300

    Update tuning.sh

--- scripts/tuning.sh
+++ scripts/tuning.sh
@@ -1,8 +1,7 @@
 #!/bin/env sh
-exit 0
+
 set -e
 
 #cp -vr ./debian-live-build/config ./
 
 cp -vr ./debian-live-build/config/package-lists/0* ./config/package-lists/
-cp -vr ./debian-live-build/config/package-lists/2* ./config/package-lists/
