88a493fbfc717d58fe3fc6e297519cbebb0382d6
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-21 03:27:51 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,11 +4,14 @@ set -e
 
 lb config noauto \
 	--color \
-	--image-name live_2 \
+	--image-name live_3 \
 	--apt-indices false \
 	--apt-recommends false \
-	--binary-image hdd \
-	--binary-filesystem ext4 \
+
+
+
+#	--binary-image hdd \
+#	--binary-filesystem ext4 \
 	
 
 
