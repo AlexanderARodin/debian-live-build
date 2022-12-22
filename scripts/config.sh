387430ea23e1762e4aa7e67fe5b0d245e6de05f9
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-22 03:18:56 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,14 +3,14 @@
 set -e
 
 	OP="--color"
-	OP="$OP --image-name max-libs"
+	OP="$OP --image-name midle"
 	#OP="$OP --binary-image hdd --binary-filesystem ext4"
 	OP="$OP --debian-installer live --debian-installer-gui false"
 	
 	#OP="$OP --apt-indices false"
 	#OP="$OP --apt-recommends false"
 	
-	OP="$OP --archive-areas \"main contrib non-free\""
-	OP="$OP --debootstrap-options \"--variant=minbase\""
+	#OP="$OP --archive-areas \"main contrib non-free\""
+	#OP="$OP --debootstrap-options \"--variant=minbase\""
 
 lb config noauto $OP
