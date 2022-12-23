f92ce2b4836459500fc630b47bb37a623f11c17c
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-23 14:51:35 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,16 +1,18 @@
 #!/bin/env sh
 
+NAME=live-rescuer
+
 set -e
 
 	OP="--color"
-	OP="$OP --image-name allarch"
+	OP="$OP --image-name $NAME"
 	#OP="$OP --binary-image hdd --binary-filesystem ext4"
-	OP="$OP --debian-installer live --debian-installer-gui false"
+	#OP="$OP --debian-installer live --debian-installer-gui false"
 	
-	#OP="$OP --apt-indices false"
-	#OP="$OP --apt-recommends false"
+	OP="$OP --apt-indices false"
+	OP="$OP --apt-recommends false"
 	
-	OP="$OP --archive-areas \"main contrib non-free\""
-	OP="$OP --debootstrap-options \"--variant=minbase\""
+	#OP="$OP --archive-areas \"main contrib non-free\""
+	#OP="$OP --debootstrap-options \"--variant=minbase\""
 
 lb config noauto $OP
