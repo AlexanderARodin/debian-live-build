57ba000c1aa11f91f2e7f17481d12aa9c182da04
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-27 23:45:35 +0300

    Update tuning.sh

--- scripts/tuning.sh
+++ scripts/tuning.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cp -vr ./debian-live-build/config ./
+cp -vr ./debian-live-build/config/*.list.chroot ./config/
 
 #cp -vr ./debian-live-build/config/package-lists/0* ./config/package-lists/
