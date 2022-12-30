eee85039a7bacd1680aebcfcd910eb178db5b63e
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-31 02:22:01 +0300

    Update launcher.sh

--- config/includes.chroot_after_packages/home/user/launcher.sh
+++ config/includes.chroot_after_packages/home/user/launcher.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env  bash
 
 cd ..
-git clone https://github.com/AlexanderARodin/raanixCreator.git
+sudo git clone https://github.com/AlexanderARodin/raanixCreator.git
 cd ./raanixCreator
-make
-chsh -s /bin/zsh
-tmux
+sudo chsh -s /bin/zsh
+sudo su
