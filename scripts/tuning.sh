64af5ce82be0ddcf65ec5b3e3f5132724cd86ea3
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-31 01:27:22 +0300

    Update tuning.sh

--- scripts/tuning.sh
+++ scripts/tuning.sh
@@ -6,7 +6,5 @@ set -e
 
 cp -vr ./debian-live-build/config/package-lists/*.list.chroot ./config/package-lists/
 
-mkdir ./config/includes.chroot_after_packages/home/
-mkdir ./config/includes.chroot_after_packages/home/user/
 chmod ug+x ./debian-live-build/config/includes.chroot_after_packages/home/user/launcher.sh
 cp -vr ./debian-live-build/config/includes.chroot_after_packages/* ./config/includes.chroot_after_packages/
