3ebf4ad8840944c34a40599edd0363217f5e36c4
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-31 00:04:10 +0300

    Update tuning.sh

--- scripts/tuning.sh
+++ scripts/tuning.sh
@@ -5,4 +5,4 @@ set -e
 #cp -vr ./debian-live-build/config ./
 
 cp -vr ./debian-live-build/config/package-lists/*.list.chroot ./config/package-lists/
-cp -vr ./debian-live-build/config/includes.chroot_after_packages/home/user/Makefile ./config/includes.chroot_after_packages/home/user/Makefile
+cp -vr ./debian-live-build/config/includes.chroot_after_packages/home/Makefile ./config/includes.chroot_after_packages/home/Makefile
