b86b9e6342e3971913e1f7c028d9aa03d0875a3d
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2022-12-30 20:50:22 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,15 +1,16 @@
 #!/bin/env sh
 
-NAME=live-rescuer
+#NAME=live-rescuer
+NAME=fleshka
 
 set -e
 
 	OP="--color"
 	OP="$OP --image-name $NAME"
-	#OP="$OP --binary-image hdd --binary-filesystem ext4"
+	OP="$OP --binary-image hdd --binary-filesystem ext2"
 	#OP="$OP --debian-installer live --debian-installer-gui false"
 	
-	OP="$OP --apt-indices false"
+	#OP="$OP --apt-indices false"
 	OP="$OP --apt-recommends false"
 	
 	#OP="$OP --archive-areas \"main contrib non-free\""
